453434a5ffc1e86df5365a1a27bcfa6d3c776bf8
makinifab <makinifarbean@gmail.com>
2024-02-09 08:14:46 -0800

    cue_ball_potted

--- main.py
+++ main.py
@@ -7,9 +7,10 @@ pygame.init()
 
 SCREEN_WIDTH = 1200
 SCREEN_HEIGHT = 678
+BOTTOM_PANEL = 50
 
 #game window
-screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT + BOTTOM_PANEL))
 pygame.display.set_caption('Mountain Top')
 
 #pymunk space
@@ -26,6 +27,7 @@ dia = 36
 taking_shot = True
 pocket_dia = 66
 force = 0
+cue_ball_potted = False
 powering_up = False
 max_force = 10000
 force_direction = 1
@@ -39,9 +41,9 @@ RED = (255, 0 , 0)
 cue_image = pygame.image.load("assets/images/cue.png").convert_alpha()
 table_image = pygame.image.load("assets/images/table.png").convert_alpha()
 ball_images = []
-for i in range(1, 17)
-ball_image = pygame.image.load(f"assets/images/ball_{i}.png").convert_alpha()
-ball_images.append(ball_image)
+for i in range(1, 17):
+    ball_image = pygame.image.load(f"assets/images/ball_{i}.png").convert_alpha()
+    ball_images.append(ball_image)
 
 
 #display pool_balls
@@ -144,6 +146,24 @@ while run:
     #draw pool table
     screen.blit(table_image, (0, 0))
 
+#check if ball is potted
+for i, ball in enumerate(balls):
+    for pocket in pockts:
+        ball_x_dist = abs(ball.body.position[0] - pocket{0})
+        ball_y_dist = abs(ball.body.position[1] - pocket{1})
+        ball_dist = math.sqrt((ball_x_dist ** 2) + (ball_y_dist ** 2))
+        if ball_dist <= pocket_dia / 2:
+            #check if potted ball was cue ball
+            if i == len(balls) - 1:
+                cue_ball_potted = True
+                ball.body.position = (-100, -100)
+                ball.body.velocity = (0.0, 0.0)
+            else:
+                space.remove(ball.body)
+                balls.remove(ball)
+                potted_balls.append(ball_images[i])
+                ball_images.pop(i)
+
     #draw pool balls
     for i, ball in enumerate(balls):
         screen.blit(ball_images[i], (ball.body.position[0] - ball.radius, ball.body.position[1] - ball.radius))
@@ -156,6 +176,10 @@ while run:
 
     #cue image
     if taking_shot == True:
+        if cue_ball_potted == True:
+            #repo cue ball
+            balls[-1].body.position = (888, SCREEN_HEIGHT / 2)
+            cue_ball_potted = False
         #calculate pool cue angle
         mouse_position = pygame.mouse.get_pos()
         cue.rect.center = balls[-1].body.position
@@ -181,6 +205,13 @@ while run:
         balls[-1].body.apply_impulse_at_local_point((force * x_impulse, force * y_impulse), (0, 0))
         force = 0
         force_direction = 1
+
+    #draw bottom panel
+    pygame.draw.rect(screen, BG, (0, SCREEN_HEIGHT, SCREEN_WIDTH, BOTTOM_PANEL))
+
+    #draw potted balls at the bottom
+    for i, ball in enumerate(potted_balls):
+        screen.blit(ball, (10 + (i * 50), SCREEN_HEIGHT + 10))
         
     #events handler
     for event in pygame.event.get():
